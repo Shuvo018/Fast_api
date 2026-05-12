51d3af86a785cfeb3245315c2ed3e610973dc684
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-12 23:31:49 +0600

    get method input id

--- main.py
+++ main.py
@@ -11,15 +11,24 @@ def hello():
 def about():
     return {"message": "Hi, i am learning fastapi"}
 
+
 def load_data():
+# load(): json -> dict | read file
+# dump(): dict -> json | write file
+
     with open('patients.json', 'r') as f:
         data = json.load(f)
-    return data if data else "lsfjaslk"
+    return data if data else "not found"
 
 @app.get("/view")
 def view():
     return load_data()
 
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
 
-# load(): json -> dict | read file
-# dump(): dict -> json | write file
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'patient not found'}
+    
