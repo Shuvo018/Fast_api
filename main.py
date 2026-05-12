5a478121cce49153e2f43a444d6e640e45c896c1
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-12 23:10:53 +0600

    list view

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
@@ -9,3 +10,16 @@ def hello():
 @app.get('/about')
 def about():
     return {"message": "Hi, i am learning fastapi"}
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data if data else "lsfjaslk"
+
+@app.get("/view")
+def view():
+    return load_data()
+
+
+# load(): json -> dict | read file
+# dump(): dict -> json | write file
