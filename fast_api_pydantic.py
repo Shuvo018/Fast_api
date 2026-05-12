0209b9ff05326d458c45e8b6b3228ba56a9aab2e
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-13 01:49:37 +0600

    more pydantic datatype and Optional

--- fast_api_pydantic.py
+++ fast_api_pydantic.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
+from typing import List, Dict, Optional
+
 
 class Patient(BaseModel):
     name: str
     age: int
+    weight: float
+    married: bool
+    allergies: Optional[List[str]] = None
+    contact_details: Dict[str, str]
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
@@ -13,10 +19,13 @@ def update_patient_data(patient: Patient):
     print(patient.age)
 
 
-patient_info = {'name': 'nitish', 'age': 21}
+patient_info1 = {'name': 'nitish', 'age': 21, 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
+patient_info2 = {'name': 'nitish', 'age': 21, 'weight': 75.2, 'married': True, 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
 
-patient1 = Patient(**patient_info)
+
+patient1 = Patient(**patient_info1)
+patient2 = Patient(**patient_info2)
 
 insert_patient_data(patient1)
 
-update_patient_data(patient1)
+update_patient_data(patient2)
