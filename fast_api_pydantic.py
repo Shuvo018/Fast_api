2a54029ec109b17490c46128cd6382dc8b3b175b
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-13 11:48:49 +0600

    Anyurl

--- fast_api_pydantic.py
+++ fast_api_pydantic.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
 
 class Patient(BaseModel):
     name: str
     email: EmailStr
+    linkedin_url: AnyUrl
     age: int
     weight: float
     married: bool = False
@@ -14,14 +15,25 @@ class Patient(BaseModel):
 def insert_patient_data(patient: Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
 
 def update_patient_data(patient: Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
 
 
-patient_info1 = {'name': 'nitish', 'email': 'abc@gmail.com', 'age': 21, 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
-patient_info2 = {'name': 'nitish', 'email': 'abc@gmail.com', 'age': 21, 'weight': 75.2, 'married': True, 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
+
+patient_info1 = {'name': 'nitish', 'email': 'abc@gmail.com', 'linkedin_url': 'http://linkedin.com/1234', 'age': 21, 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
+patient_info2 = {'name': 'nitish', 'email': 'abc@gmail.com', 'linkedin_url': 'http://linkedin.com/1234', 'age': 21, 'weight': 75.2, 'married': True, 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
 
 
 patient1 = Patient(**patient_info1)
