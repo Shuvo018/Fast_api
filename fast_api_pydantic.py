59535c1e7c3e92b905c4a58c06a31d69b8197ae3
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-13 11:45:34 +0600

    EmailStr

--- fast_api_pydantic.py
+++ fast_api_pydantic.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List, Dict, Optional
 
 
 class Patient(BaseModel):
     name: str
+    email: EmailStr
     age: int
     weight: float
     married: bool = False
@@ -19,8 +20,8 @@ def update_patient_data(patient: Patient):
     print(patient.age)
 
 
-patient_info1 = {'name': 'nitish', 'age': 21, 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
-patient_info2 = {'name': 'nitish', 'age': 21, 'weight': 75.2, 'married': True, 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
+patient_info1 = {'name': 'nitish', 'email': 'abc@gmail.com', 'age': 21, 'weight': 75.2, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
+patient_info2 = {'name': 'nitish', 'email': 'abc@gmail.com', 'age': 21, 'weight': 75.2, 'married': True, 'contact_details': {'email': 'abc@gmail.com', 'phone': '01888'}}
 
 
 patient1 = Patient(**patient_info1)
